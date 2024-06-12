3ae79357040421682e414bef203f9b404d8e3d55
Miguel Loureiro <miguel.silvaloureiro@gmail.com>
2024-06-12 01:08:09 +0100

    Add SUPER and GUEST; and also method to get authorities

--- BookCatalog/bookcatalog/src/main/java/com/bookcatalog/bookcatalog/model/Role.java
+++ BookCatalog/bookcatalog/src/main/java/com/bookcatalog/bookcatalog/model/Role.java
@@ -1,7 +1,19 @@
 package com.bookcatalog.bookcatalog.model;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 public enum Role {
 
+    SUPER,
     ADMIN,
-    READER
+    READER,
+    GUEST;
+
+        public List<SimpleGrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + this.name()));
+    }
+
 }
--- BookCatalog/bookcatalog/src/main/java/com/bookcatalog/bookcatalog/repository/UserRepository.java
+++ BookCatalog/bookcatalog/src/main/java/com/bookcatalog/bookcatalog/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package com.bookcatalog.bookcatalog.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
-
 import com.bookcatalog.bookcatalog.model.User;
 
 public interface UserRepository extends CrudRepository<User, Long>{
